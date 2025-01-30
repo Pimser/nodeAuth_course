0968e67035010619a80e9861bf6df4ffe3f8a6c9
Pimser <williamhellebustkromvoll@gmail.com>
2025-01-30 13:45:18 +0100

    torsdag la til jwt tokens og singup

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,3 +23,23 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+//cookies
+app.get("/set-cookies", (req, res) => {
+
+  // res.setHeader("Set-Cookie", "newUser=true");
+
+  res.cookie("newUser", false) //første argument er navn og andre argument er verdi
+  res.cookie("isEmployee", true, {maxAge: 1000 * 60 * 60 * 24,  }); //tredje argument er tiden cookien varer //(secure: true) = secure er https og ikke http
+  res.send("you got the cookie!");
+
+});
+
+app.get("/read-cookies", (req, res) =>{
+
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+
+});
